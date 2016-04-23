b4263a519be91ea28cbdfe737e69919fc572a26f
Yudai Hashimoto <jp7fkf@gmail.com>
2016-04-23 23:45:56 +0900

    modified sigital swr meter

--- digital_swr_meter.X/main.c
+++ digital_swr_meter.X/main.c
@@ -1,7 +1,7 @@
 #define _XTAL_FREQ 8000000
 #define INTERCEPT -84-30 //for calibration
 #define TANGENT 25 //for calibration
-#define REF_mV 2405 //ADC reference voltage
+#define REF_mV 5000 //ADC reference voltage
 #define RES 1024 //resolution of ADC bits
 #define RES_mV REF_mV/RES //resolution of ADC voltage
 
@@ -16,17 +16,13 @@
 
 // PIC16F88 Configuration Bit Settings
 
-// 'C' source line config statements
-// #pragma config statements should precede project file includes.
-// Use project enums instead of #define for ON and OFF.
-
 // CONFIG1
 #pragma config FOSC = INTOSCIO  // Oscillator Selection bits (INTRC oscillator; port I/O function on both RA6/OSC2/CLKO pin and RA7/OSC1/CLKI pin)
-#pragma config WDTE = ON        // Watchdog Timer Enable bit (WDT enabled)
-#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
 #pragma config MCLRE = OFF      // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is digital I/O, MCLR internally tied to VDD)
 #pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
-#pragma config LVP = ON         // Low-Voltage Programming Enable bit (RB3/PGM pin has PGM function, Low-Voltage Programming enabled)
+#pragma config LVP = OFF        // Low-Voltage Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
 #pragma config CPD = OFF        // Data EE Memory Code Protection bit (Code protection off)
 #pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off)
 #pragma config CCPMX = RB0      // CCP1 Pin Selection bit (CCP1 function on RB0)
@@ -42,34 +38,36 @@ char sprint_double(char *str, int str_length, double val, int digit);
 void main(void)
 {
     init();
-    double fwd, ref;
+    int fwd, ref;
     double fwd_watt, ref_watt;
     double fwd_sqrt, ref_sqrt;
-    double swr;
+    double swr=0;
     char str[6];
-
+    
     while(1)
     {
-        fwd = adc_read(1);
+        fwd = adc_read(0);
         ref = adc_read(1);
-
+        
         //bits -> Voltage -> dBm -> Watt
-        fwd_watt = pow(10, (TANGENT * fwd * RES_mV - INTERCEPT)/10);
-        ref_watt = pow(10, (TANGENT * ref * RES_mV - INTERCEPT)/10);
-
+        fwd_watt = 100;//pow(10, (TANGENT * fwd * RES_mV - INTERCEPT)/10);
+        ref_watt = 10;//pow(10, (TANGENT * ref * RES_mV - INTERCEPT)/10);
+        
+        fwd_sqrt = pow(fwd_watt, 0.5);
+        ref_sqrt = pow(ref_watt, 0.5);
         //fwd_sqrt = sqrt(fwd_watt);
         //ref_sqrt = sqrt(ref_watt);
 
         swr = (fwd_sqrt + ref_sqrt)/(fwd_sqrt - ref_sqrt);
-
+        
         lcd_goto(0x00);
         __delay_us(50);
-        sprint_double(str, 6, fwd_watt, 2);
+        sprint_double(str, 6, fwd_watt, 1);
         lcd_puts(str);
         __delay_us(50);
         lcd_puts(" / ");
         __delay_us(50);
-        sprint_double(str, 6, ref_watt, 2);
+        sprint_double(str, 6, ref_watt, 1);
         lcd_puts(str);
         __delay_us(50);
         
@@ -84,12 +82,13 @@ void main(void)
 void init()
 {
     CMCON = 0b00000111; //cut off analog comparator just add this one
+    INTCON = 0; // purpose of disabling the interrupts.
+    OSCCON = 0b01111010;
     TRISA = 0b00000011; //ra0 and ra1 as input
     TRISB = 0b00000000;
     PORTA = 0b00000000;
     PORTB = 0b00000000;
-    ANSEL = 0b00001111;
-    INTCON=0; // purpose of disabling the interrupts.
+    ANSEL = 0b00000011;
 
     lcd_init();
     adc_init();
@@ -101,7 +100,8 @@ void init()
     lcd_goto(0x40);	// Select second line
     __delay_us(50);
     lcd_puts("      METER     ");
-    __delay_ms(30);
+    __delay_ms(1500);
+    lcd_clear();
 }
 
 char sprint_double(char *str, int str_length, double val, int digit)
